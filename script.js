cf3dce92ae2ae222f2e5d915fc8ddf72d2fcfbed
melfinitamba <141936253+melfinitamba@users.noreply.github.com>
2023-08-31 23:26:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function replaceName (){
+let name = prompt{Siapakah nama anda}", ";
+document.getElementById("name").innerHTML= name
+}
+
+document.getElementById('tombol')
+tombol.addEventListener {"click", function(){
+    replaceName()
+}}
